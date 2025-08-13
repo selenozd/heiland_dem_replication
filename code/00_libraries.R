1185e29a4edc75b079f9792c46b1f2342a9caf19
selenozd <selenozd@gmail.com>
2025-08-13 14:01:54 -0700

    Create 00_libraries.R

---
+++ code/00_libraries.R
@@ -0,0 +1,24 @@
+
+### Extreme Weather and Mortality of Vulnerable Urban Populations:  
+### An Examination of Temperature and Unclaimed Deaths in New York City 
+
+### Load (or Install) Required R Packages
+### Author: Selen Ozdogan
+
+# check for and install pacman if not already installed
+if (!require("pacman")) {
+  install.packages("pacman")
+  library(pacman)
+}
+
+# define and load standard libraries
+packages <- c("tidyverse", "devtools", "furrr", "readr", "tibble", "dplyr",
+             "tictoc", "glue", "lubridate", "magrittr", "janitor", "units", 
+             "sjmisc", "fixest", "foreign", "haven", "readxl", "stringdist", 
+             "ggpubr", "ggExtra", "sandwich", "modelsummary", "jtools", 
+             "huxtable", "here", "stargazer", "MASS", "terra", "rgdal", 
+             "raster", "gdalUtilities", "exactextractr", "sf", "tmap", 
+             "tmaptools", "stars", "rmapshaper", "animation",
+             "mapview", "ggmap", "geosphere")
+
+p_load(char = packages)
